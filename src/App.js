e3ba71f62a865804d0626cff1ca4ac1537d0d5b9
Rigoberto Torres <rigoberto.torres@gl.local>
2019-06-27 14:06:37 -0400

    finalizacion de proyecto

--- src/App.js
+++ src/App.js
@@ -1,30 +1,44 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import Header from "./components/Header";
+import ListaNoticias from "./components/ListaNoticias";
+import Formulario from "./components/Formulario";
 
 class App extends Component {
     state = {
         noticias: []
     }
 
-    async componentDidMount() {
+    componentDidMount() {
+        this.consultarNoticias();
+    }
+    
+    consultarNoticias = async (categoria = 'general') => {
         const token = "8251c17b02ea44db893f8d2af1f671e7"
-        const url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=${token}`
+        const url = `https://newsapi.org/v2/top-headlines?country=mx&category=${categoria}&apiKey=${token}`
 
         const respuesta = await fetch(url);
         const noticias = await respuesta.json();
 
-        console.log(noticias);
-
         this.setState({
-            noticias
+            noticias : noticias.articles
         })
-
     }
-    
+
     render() {
         return (
-            <div>
-                
-            </div>
+            <Fragment>
+                <Header
+                    titulo="Noticias React API"
+                />
+                <div className="container white contenedor-noticias ">
+                    <Formulario 
+                        consultarNoticias={this.consultarNoticias}
+                    />
+                    <ListaNoticias
+                        noticias={this.state.noticias}
+                    />
+                </div>
+            </Fragment>
         );
     }
 }
